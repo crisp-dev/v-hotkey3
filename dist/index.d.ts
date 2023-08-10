5332b44f0d5aeaafb5190c98d294b4d87d92bd35
Eliott Vincent <dev@eliottvincent.com>
2023-08-10 11:34:19 +0200

    Bump

---
+++ dist/index.d.ts
@@ -0,0 +1,22 @@
+import { Directive, Plugin } from 'vue';
+
+interface HTMLElementWithKeyMap extends HTMLElement {
+    _keyMap: Record<any, any>[];
+    _keyHandler: (e: Event) => Event | undefined;
+}
+interface SVGElementWithKeyMap extends SVGElement {
+    _keyMap: Record<any, any>[];
+    _keyHandler: (e: Event) => Event | undefined;
+}
+declare function buildDirective(alias: Record<string, string | number>): Directive<HTMLElementWithKeyMap | SVGElementWithKeyMap>;
+
+type KeyMap = Record<string, Record<string, () => void> | (() => void)>;
+interface Options {
+    modifier?: 'prevent' | 'stop';
+    alias?: Record<string, string | number>;
+}
+declare function useHotkey(keymap: KeyMap, options?: Options): void;
+
+declare const HotkeyPlugin: Plugin;
+
+export { buildDirective as HotkeyDirective, HotkeyPlugin as default, useHotkey };
